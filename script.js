3a6ea3fd6eaa71f22af0adb55e6a870b9d1ee6cd
Anmol Malhan <malhananmol1@gmail.com>
2025-07-19 18:39:49 +0530

    added some more examples

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ function truncateString(str, num) {
         return str.slice(0, num) + '...';
     }
 }
-//some examples
+// some examples
 console.log ( truncateString("A-tisket a-tasket A green and yellow basket", 8) );
 console.log ( truncateString("Peter Piper picked a peck of pickled peppers", 11) );
 console.log( truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length) );
 console.log ( truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) );
 console.log( truncateString("A-", 1) );
 console.log ( truncateString("Absolutely Longer", 2) );
+console.log( truncateString("A-tisket a-tasket A green and yellow basket", 0) );
